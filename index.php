67381d5074121b5fb73de65fc46edb7fc141351c
buhala <fix288@gmail.com>
2013-11-29 23:10:05 +0200

    Initial commit
    A temp design has been created

---
+++ index.php
@@ -0,0 +1,26 @@
+﻿<!doctype html>
+<html>
+<head>
+<title>Христо Димитров | Портфолио</title>
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<div id="content">
+<div id="heading">Христо Димитров</div>
+<div id="slogan">Уеб разработчик на свободна практика</div>
+
+<hr>
+<div id="menu"><img src="img/about.png" width="30"><img src="img/contact.png" width="30"><img src="img/blog.png" width="30"></div>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+<br>
+<div id="footer">
+    <img src="img/facebook.png" alt="Facebook" width="30">
+    <img src="img/twitter.png" alt="Twitter" width="30">
+    <img src="img/gplus.png" alt="Google Plus" width="30">
+    <img src="img/github.png" alt="Github" width="30">
+    <div class="right">&copy; 2013 Всички права запазени.</div>
+    </div>
+</div>
+</body>
+</html>
